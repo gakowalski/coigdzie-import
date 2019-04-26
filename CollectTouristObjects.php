c8f6a593fdd9407142c9e003a3f8cb0df4884e2d
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2019-04-26 14:10:18 +0200

    support for ID restrictions

--- CollectTouristObjects.php
+++ CollectTouristObjects.php
@@ -179,6 +179,9 @@ class CollectTouristObjects {
     $category_mappings = $this->get_category_mappings();
 
     foreach ($happenings as $happening) {
+      if (in_array($happening['happeningId'], $restrictions['ids'] === false) {
+        continue;
+      }
       if ($restrictions['date_from'] !== null && strtotime($happening['modified']) < strtotime($restrictions['date_from'])) {
         continue;
       }
